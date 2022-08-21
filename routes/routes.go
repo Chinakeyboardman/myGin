16c3ff6903fe8deca6d7d5401f63f9e54010f398
chinakeyboardman <chinakeyboardman@outlook.com>
2022-08-21 23:24:20 +0800

    路由重构,可以安装路由组和路由层级来进行配置,就像laravel一样

--- routes/routes.go
+++ routes/routes.go
@@ -1,6 +1,6 @@
 package routes
 
-//routes/routes.go
+////routes/router.go
 
 import (
 	"myGin/context"
@@ -10,9 +10,98 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
+// 这里可以按照层级结构配置路由还有路由组
 func Load(r *gin.Engine) {
 
-	r.GET("/", convert(controller.Index))
+	router := newRouter(r)
+
+	router.Group("/api", func(api group) {
+
+		api.Registered(GET, "/info", controller.Index)
+
+		api.Group("/user", func(user group) {
+
+			user.Registered(GET, "/info", controller.Index)
+			user.Registered(GET, "/order", controller.Index)
+			user.Registered(GET, "/money", controller.Index)
+
+		})
+
+	})
+
+}
+
+type router struct {
+	engine *gin.Engine
+}
+
+type group struct {
+	engine *gin.Engine
+	path   string
+}
+
+type method int
+
+const (
+	GET    method = 0x000000
+	POST   method = 0x000001
+	PUT    method = 0x000002
+	DELETE method = 0x000003
+	ANY    method = 0x000004
+)
+
+func newRouter(engine *gin.Engine) *router {
+
+	return &router{
+		engine: engine,
+	}
+}
+
+func (r *router) Group(path string, callback func(group)) {
+
+	callback(group{
+		engine: r.engine,
+		path:   path,
+	})
+
+}
+
+func (g group) Group(path string, callback func(group)) {
+
+	g.path += path
+
+	callback(g)
+}
+
+func (g group) Registered(method method, url string, action func(*context.Context) *response.Response) {
+
+	handlerFunc := convert(action)
+
+	finalUrl := g.path + url
+
+	switch method {
+
+	case GET:
+
+		g.engine.GET(finalUrl, handlerFunc)
+
+	case POST:
+
+		g.engine.GET(finalUrl, handlerFunc)
+
+	case PUT:
+
+		g.engine.PUT(finalUrl, handlerFunc)
+
+	case DELETE:
+
+		g.engine.DELETE(finalUrl, handlerFunc)
+
+	case ANY:
+
+		g.engine.Any(finalUrl, handlerFunc)
+
+	}
 
 }
 
